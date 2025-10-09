58e745cd6c2ca0315066a1d6f5f1c93ba0867224
Jomikael Ruiz <92127962+Samuriot@users.noreply.github.com>
2025-10-08 23:12:45 -0700

    Added new images & local pathing to fix assets folder (#2)
    * Added base angular files + hifi
    
    * Added new images & local pathing to fix assets folder

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,8 +11,8 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'synceq';
   cards = [
-  { image: 'https://media.licdn.com/dms/image/v2/D5603AQGYpyQ3-YpSaQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1672868194402?e=1762992000&v=beta&t=n_Du6Mdr0QgNpmpRY-sunEPhSIqmqIWvdpxBSNgUB3A', username: 'HiImKimchi', text: 'Sample card 1', genre: 'EDM' },
-  { image: 'https://media.licdn.com/dms/image/v2/D5603AQHOeL3fYFhTZg/profile-displayphoto-crop_800_800/B56ZhTCdBDHUAM-/0/1753739813067?e=1762992000&v=beta&t=LCIPBV_9a5kRZh-5FQrW2iR-Gu1a9BZrELtBG8lQhC4', username: 'Samuriot', text: 'Sample card 1', genre: 'K-Pop' },
-  { image: 'https://media.licdn.com/dms/image/v2/D4E03AQGzc6TDDLuE5g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1691470054000?e=1762992000&v=beta&t=B1g_IaYEyb69PvJ8GHl0LXVwa9VA_pWloP2wfD958bo', username: 'BADevera04', text: 'Sample card 1', genre: 'RnB' },
+    { image: 'assets/images/bryan.jpg', username: 'HiImKimchi', genre: 'EDM' },
+    { image: 'assets/images/jomi.jpg', username: 'Samuriot', genre: 'K-Pop' },
+    { image: 'assets/images/ben.jpg', username: 'BADevera04', genre: 'RnB' },
   ];
 }
