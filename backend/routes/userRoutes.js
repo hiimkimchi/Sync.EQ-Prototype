ace54f49aa86da32069e94b328050ef6fe99912c
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-10-20 12:58:11 -0700

    fixed type

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -29,7 +29,7 @@ app.get("/:username", async (req, res) => {
 app.get("/", async (req, res) => {
     try {
         const users = await User.find();
-        if(!user) {
+        if(!users) {
             return res.status(404).json({ error: "No users in db" });
         }
         res.status(200).json(users);
