6d62fb39435994270d29539de16448a5965db894
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-10-23 19:32:56 -0700

    updated urls to use ENV files + added error checks

--- frontend/src/app/pages/callback/callback.component.ts
+++ frontend/src/app/pages/callback/callback.component.ts
@@ -1,12 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router'
 import { AuthService } from '@auth0/auth0-angular';
 import { ReactiveFormsModule } from '@angular/forms';
-import { filter, switchMap } from 'rxjs';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { userInfo } from 'os';
+import { ENV } from '../../core/environment';
 
 @Component({
     selector: 'app-create-profile',
@@ -25,15 +23,17 @@ export class CallbackPage implements OnInit {
 
     // initiates when a page is loaded
     ngOnInit(): void {
+        const api_url = ENV.apiUrl + "/api/users/get";
         if(this.auth.isAuthenticated$) {    
             this.auth.user$.subscribe({
                 next: (res) => {
-                this.httpC.get(`http://localhost:3000/api/users/get/${res?.nickname}`).subscribe({
+                this.httpC.get(`${api_url}/${res?.nickname}`).subscribe({
                     next: (response) => {
                         this.router.navigateByUrl("/");
                     },
                     error: (error) => {
-                        this.router.navigateByUrl("/create");  
+                        if(error.status === 404) 
+                            this.router.navigateByUrl("/create");  
                     },
                 })
                 }
