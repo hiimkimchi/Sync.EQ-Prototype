8039cc4fcbaeb929e369633adbd155281b0cf12f
Bryan Kim <wabkim360@gmail.com>
2025-10-27 01:37:16 -0700

    bruh

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { setEnv } from './app/core/environment';
 import { appConfig } from './app/core/app.config';
 
 fetch('/assets/config.json')
   .then((res) => res.json())
   .then((config) => {
-    setEnv(config);
     return bootstrapApplication(AppComponent, appConfig);
   })
   .catch((err) => console.error('Failed to load config.json', err));
