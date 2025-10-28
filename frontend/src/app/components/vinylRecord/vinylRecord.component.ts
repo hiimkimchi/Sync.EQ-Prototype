572b1cab1315b68682b116bfdcf31213054848b3
Bryan Kim <wabkim360@gmail.com>
2025-10-28 15:33:36 -0700

    profile better

--- frontend/src/app/components/vinylRecord/vinylRecord.component.ts
+++ frontend/src/app/components/vinylRecord/vinylRecord.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import { bootstrapSearch, bootstrapHouse } from '@ng-icons/bootstrap-icons';
-import { AuthButtonComponent } from '../AuthButtonComponent/AuthButtonComponent.component';
 
 @Component({
   selector: 'vinyl-record',
-  imports: [CommonModule, NgIconComponent, RouterModule, AuthButtonComponent],
+  imports: [CommonModule, RouterModule],
   providers: [provideIcons({ bootstrapSearch, bootstrapHouse })],
   templateUrl: './vinylRecord.component.html',
 })
