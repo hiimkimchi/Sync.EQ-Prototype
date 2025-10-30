51921aad4b9677bce41640a0333a2c2e5ed43ce2
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-10-30 15:43:47 -0700

    fixed postRoutes to use postController

--- backend/routes/postRoutes.ts
+++ backend/routes/postRoutes.ts
@@ -6,8 +6,8 @@ const app = express.Router();
 app.post("/", pc.createPost);
 
 //get a post via unique post id
-app.get("/:username/:postId", pc.getSpecificPost);
-app.get("/:username", pc.getUsersPosts);
 app.get("/", pc.getAllPosts);
+app.get("/:username", pc.getUsersPosts);
+app.get("/:username/:postId", pc.getSpecificPost);
 
 export default app;
