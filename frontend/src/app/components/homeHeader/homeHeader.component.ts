53829fb2ef3076967787d4dea6355b18fe68e871
Benjamin Arthur de Vera <141700197+badevera@users.noreply.github.com>
2025-11-06 10:46:14 -0800

    Adjusted Nav bar

--- frontend/src/app/components/homeHeader/homeHeader.component.ts
+++ frontend/src/app/components/homeHeader/homeHeader.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { bootstrapSearch, bootstrapHouse } from '@ng-icons/bootstrap-icons';
+import { bootstrapSearch, bootstrapHouse, bootstrapListUl } from '@ng-icons/bootstrap-icons';
 import { AuthButtonComponent } from '../AuthButtonComponent/AuthButtonComponent.component';
 
 
 @Component({
   selector: 'synceq-header',
   imports: [CommonModule, NgIconComponent, RouterModule, AuthButtonComponent],
-  providers: [provideIcons({ bootstrapSearch, bootstrapHouse })],
+  providers: [provideIcons({ bootstrapSearch, bootstrapHouse, bootstrapListUl })],
   templateUrl: './homeHeader.component.html',
 })
 export class SynceqHeader {
