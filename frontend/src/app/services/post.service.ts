84a1cd1533915a5ccfdbdc12a834793e6fdfa85c
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-11-06 19:05:13 -0800

    Added Post interface + updated controller to filter out data

---
+++ frontend/src/app/services/post.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/users';
+import { Post } from '../models/post';
+import { ENV } from '../core/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+    private baseUrl = `${ENV.apiUrl}api/posts`;
+
+    constructor(private http: HttpClient) { }
+
+    getSpecificUserPosts(username: string): Observable<Post> {
+        return this.http.get<Post>(`${this.baseUrl}/${username}`);
+    }
+
+    // will return an array of maxSize 20
+    getAllPosts(): Observable<Post> {
+        return this.http.get<Post>(`${this.baseUrl}`);
+    }
+
+    createPost(username: string, data: Partial<Post>): Observable<Post> {
+        return this.http.post<Post>(`${this.baseUrl}/`, data);
+    }
+}
