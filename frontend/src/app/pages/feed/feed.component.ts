7a926b60f5232059fd79f42ddde1f067287861c6
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-11-06 23:35:31 -0800

    integrated api call to backend to pull and get the most recent posts

--- frontend/src/app/pages/feed/feed.component.ts
+++ frontend/src/app/pages/feed/feed.component.ts
@@ -1,20 +1,29 @@
 import { SynceqHeader } from '../../components/homeHeader/homeHeader.component';
 import { SynceqFooter } from '../../components/homeFooter/homeFooter.component';
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../models/users';
+import { Post } from '../../models/post';
+import { PostService } from '../../services/post.service';
+import { PostCard } from '../../components/postCard/postCard.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SynceqHeader, SynceqFooter],
+  imports: [SynceqHeader, SynceqFooter, PostCard],
   providers: [],
   templateUrl: './feed.component.html',
 })
 export class FeedPage implements OnInit {
     title = 'explore-page';
-    auth0Info = {} as User ;
+    posts?: [Post];
     
-    constructor() {};
+    constructor(private postService: PostService
+    ) {};
 
     ngOnInit(): void {
+      this.postService.getAllPosts().subscribe({
+        next: (data) => {
+          console.log(data);
+          this.posts = data;
+        }
+      });
     }
 }
