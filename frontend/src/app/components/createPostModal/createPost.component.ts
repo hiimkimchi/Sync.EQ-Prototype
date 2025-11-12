4822a4975c0b25afe6cfcd11bae4956a80244f34
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-11-12 11:51:34 -0800

    Created createPostModals for dialog boxes and submitting request to backend

---
+++ frontend/src/app/components/createPostModal/createPost.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'create-post-modal',
+  templateUrl: './createPost.component.html',
+  imports: [FormsModule]
+})
+export class CreatePostModal {
+  @Input() user?: any;
+  @Output() closed = new EventEmitter<void>(); // notify parent when modal closes
+
+  title = '';
+  content = '';
+
+  constructor(private postService: PostService) {}
+
+  submitPost() {
+    const postData = { 
+        postId: "67",
+        title: this.title, 
+        content: this.content, 
+        author: this.user?.username 
+    };
+    this.postService.createPost(this.user.username, postData).subscribe({
+      next: (res) => {
+        console.log('Post created', res);
+        this.closeModal();
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  closeModal() {
+    this.closed.emit();
+  }
+}
