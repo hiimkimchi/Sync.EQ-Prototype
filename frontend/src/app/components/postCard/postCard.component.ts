d057df300deaf3642dd15a7f09094521173862b6
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-11-11 22:37:32 -0800

    Added functionality to like & repost buttons

--- frontend/src/app/components/postCard/postCard.component.ts
+++ frontend/src/app/components/postCard/postCard.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Post } from '../../models/post';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'post-card',
@@ -10,4 +11,31 @@ import { Post } from '../../models/post';
 export class PostCard {
     title = 'post-card';
     @Input() post?: any;
+    @Input() user?: any;
+
+    constructor(private postService: PostService) {}
+
+    addLike() {
+      if(!this.user) {
+        console.log("user not logged in")
+        return;
+      }
+      if(this.post.likes_usernames?.includes(this.user.username)) {
+        return;
+      }
+      this.post.likes_usernames?.push(this.user.username);
+      this.postService.updatePost(this.post);
+    }
+
+    addRepost() {
+      if(!this.user) {
+        console.log("user not logged in")
+        return;
+      }
+      if(this.post.reposts_usernames?.includes(this.user.username)) {
+        return;
+      }
+      this.post.reposts_usernames?.push(this.user.username);
+      this.postService.updatePost(this.post);
+    }
 }
