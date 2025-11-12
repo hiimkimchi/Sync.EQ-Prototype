37a6386aa7a9ca69f6496be9208024e508166e2a
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-11-11 22:05:26 -0800

    updated the backend API calls to accurately fill Post interfaces

--- frontend/src/app/services/post.service.ts
+++ frontend/src/app/services/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Post } from '../models/post';
 import { ENV } from '../core/environment';
 
@@ -12,13 +12,25 @@ export class PostService {
 
     constructor(private http: HttpClient) { }
 
-    getSpecificUserPosts(username: string): Observable<Post> {
-        return this.http.get<Post>(`${this.baseUrl}/${username}`);
+    private filterTimeStamps(post: Post): Post {
+        return {
+            ...post,
+            createdAt: post.createdAt ? new Date(post.createdAt) : undefined,
+            updatedAt: post.updatedAt ? new Date(post.updatedAt) : undefined
+        };
+    }
+
+    getSpecificUserPosts(username: string): Observable<Post[]> {
+        return this.http.get<Post[]>(`${this.baseUrl}/${username}`)
+            .pipe(map(posts=>posts.map(p=>this.filterTimeStamps(p))));
+
     }
 
     // will return an array of maxSize 20
-    getAllPosts(): Observable<[Post]> {
-        return this.http.get<[Post]>(`${this.baseUrl}`);
+    getAllPosts(): Observable<Post[]> {
+        return this.http
+            .get<Post[]>(`${this.baseUrl}`)
+            .pipe(map( posts=>posts.map ( p=>this.filterTimeStamps(p) )));
     }
 
     createPost(username: string, data: Partial<Post>): Observable<Post> {
