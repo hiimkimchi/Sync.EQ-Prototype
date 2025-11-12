b62d347ed3031f52d94fb2eb0e67355a78f15a75
Jomikael Ruiz <92127962+Samuriot@users.noreply.github.com>
2025-11-11 22:48:30 -0800

    Feed Creation (#47)
    * fixed some routing to temporary pages (the other links will be updated at a later time)
    
    * Created the feed components for Angular to understand
    
    * Added Post interface + updated controller to filter out data
    
    * removed unneeded import
    
    * updated post.service.ts to fix getAllPosts()
    
    * Updated postCard with sample templating code to be updated later
    
    * integrated api call to backend to pull and get the most recent posts
    
    * Added carousel to feed to help with scrolling through posts
    
    * Added a flag for post creation date
    
    * updated the backend API calls to accurately fill Post interfaces
    
    * Fixed feed issues with auto-rolling on post carousel
    
    * Redesigned the posts to have a simple display
    
    * Added an updatePost method to be used later on
    
    * Added functionality to like & repost buttons

---
+++ frontend/src/app/services/post.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Post } from '../models/post';
+import { ENV } from '../core/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+    private baseUrl = `${ENV.apiUrl}api/posts`;
+
+    constructor(private http: HttpClient) { }
+
+    private filterTimeStamps(post: Post): Post {
+        return {
+            ...post,
+            createdAt: post.createdAt ? new Date(post.createdAt) : undefined,
+            updatedAt: post.updatedAt ? new Date(post.updatedAt) : undefined
+        };
+    }
+
+    getSpecificUserPosts(username: string): Observable<Post[]> {
+        return this.http.get<Post[]>(`${this.baseUrl}/${username}`)
+            .pipe(map(posts=>posts.map(p=>this.filterTimeStamps(p))));
+
+    }
+
+    // will return an array of maxSize 20
+    getAllPosts(): Observable<Post[]> {
+        return this.http
+            .get<Post[]>(`${this.baseUrl}`)
+            .pipe(map( posts=>posts.map ( p=>this.filterTimeStamps(p) )));
+    }
+
+    createPost(username: string, data: Partial<Post>): Observable<Post> {
+        return this.http.post<Post>(`${this.baseUrl}/`, data);
+    }
+
+    updatePost(post: Post): Observable<Post> {
+        return this.http.post<Post>(`${this.baseUrl}/`, post);
+    }
+}
