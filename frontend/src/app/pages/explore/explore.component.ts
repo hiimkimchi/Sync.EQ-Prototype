ad9a5ea17b35ddea8952425deae12a91f2eee5e4
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-11-13 16:09:54 -0800

    extended explore to use params

--- frontend/src/app/pages/explore/explore.component.ts
+++ frontend/src/app/pages/explore/explore.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import { bootstrapMusicNoteBeamed } from '@ng-icons/bootstrap-icons';
 import { ProfileCard } from '../../components/profileCard/profileCard.component';
 import { SynceqHeader } from '../../components/homeHeader/homeHeader.component';
@@ -8,6 +8,8 @@ import { SynceqFooter } from '../../components/homeFooter/homeFooter.component';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { User } from '../../models/users';
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -16,29 +18,44 @@ import { User } from '../../models/users';
   templateUrl: './explore.component.html',
 })
 export class ExplorePage implements OnInit {
-    title = 'explore-page';
-    auth0Info = {} as User ;
-    cards = [
-        { image: 'assets/images/bryan.jpg', username: 'HiImKimchi', genre: 'EDM' },
-        { image: 'assets/images/jomi.jpg', username: 'Samuriot', genre: 'K-Pop' },
-        { image: 'assets/images/ben.jpg', username: 'BADevera04', genre: 'RnB' },
-    ];
-    
-    constructor(private router : Router,
-                private auth : AuthService
-    ) {};
+  title = 'explore-page';
+  auth0Info = {} as User;
+  filters = {
+    username: '',
+    genre: '',
+    professions: [] as string[]
+  };
+  cards:User[];
 
-    ngOnInit(): void {
-      if(this.auth.isAuthenticated$) {    
-        this.auth.user$.subscribe({
-          next: (res) => {
-            this.auth0Info.username = res?.nickname;
-          }
-        });
-      }
-    }
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.cards = [];
+  }
 
-    routeToProfile(): void {
-      this.router.navigate(['/profile', this.auth0Info.username]);
+  ngOnInit(): void {
+    if (this.auth.isAuthenticated$) {
+      this.auth.user$.subscribe({
+        next: (res) => {
+          this.auth0Info.username = res?.nickname;
+        },
+      });
     }
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.filters.username = params['username'];
+      this.filters.genre = params['genre'];
+      this.filters.professions = params['professions']
+      this.userService.getFilteredProfiles(this.filters).subscribe({
+        next: (res) => this.cards = res,
+        error: (err) => console.error('Error fetching profiles', err)
+      });
+    });
+  }
+
+  routeToProfile(): void {
+    this.router.navigate(['/profile', this.auth0Info.username]);
+  }
 }
