93b637dc63d2a01996680e42aed5a60d9e6bfe99
Benjamin Arthur de Vera <141700197+badevera@users.noreply.github.com>
2025-11-13 16:38:37 -0800

    Jest Testing (#53)

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User from "../models/user.js";
+import User from "../models/user";
 
 export async function getUsers(req: Request, res: Response) {
     try {
