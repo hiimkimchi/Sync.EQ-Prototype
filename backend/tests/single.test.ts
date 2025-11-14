88d371d2659688ef5d3a45962d04c2fffed8b05c
Benjamin Arthur de Vera <141700197+badevera@users.noreply.github.com>
2025-11-13 16:45:32 -0800

    c

--- backend/tests/single.test.ts
+++ backend/tests/single.test.ts
@@ -1,5 +1,5 @@
-import User from './models/user';
-import { getSpecificUser} from './controllers/userController';
+import User from '../models/user';
+import { getSpecificUser} from '../controllers/userController';
 import { Request, Response } from 'express';
 
 
