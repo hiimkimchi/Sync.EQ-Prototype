259f428fd6c5b6bdf3cda5c2251393b2bb01f2a2
Bryan Kim <wabkim360@gmail.com>
2025-12-03 00:59:09 -0800

    add animation provider to config

--- frontend/src/app/core/app.config.ts
+++ frontend/src/app/core/app.config.ts
@@ -1,18 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAuth0 } from '@auth0/auth0-angular';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { ENV } from './environment';
-
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
+    provideAnimations(),
     provideAuth0({
       domain: ENV.auth0Domain,
       clientId: ENV.auth0ClientId,
