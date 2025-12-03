6d7f454171283ad969fff738881a1e2994543261
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-12-02 19:03:58 -0800

    bug fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/core/app.config';
 
-fetch('/assets/config.json')
-  .then((res) => res.json())
-  .then((config) => {
-    return bootstrapApplication(AppComponent, appConfig);
-  })
-  .catch((err) => console.error('Failed to load config.json', err));
+bootstrapApplication(AppComponent, appConfig);
+
+// fetch('/assets/config.json')
+//   .then((res) => res.json())
+//   .then((config) => {
+//     return bootstrapApplication(AppComponent, appConfig);
+//   })
+//   .catch((err) => console.error('Failed to load config.json', err));
