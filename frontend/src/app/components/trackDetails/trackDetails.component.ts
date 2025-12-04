89b8ac4357455f6b6c1b30723a14a41744f37f6e
Bryan Kim <wabkim360@gmail.com>
2025-12-04 02:06:59 -0800

    inject username; make download functionality

--- frontend/src/app/components/trackDetails/trackDetails.component.ts
+++ frontend/src/app/components/trackDetails/trackDetails.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MediaAudioService } from '../../services/media/audio.service';
 
 @Component({
   selector: 'track-details-panel',
@@ -11,4 +12,33 @@ import { CommonModule } from '@angular/common';
 export class TrackDetails {
   @Input() track: any = null;
   @Input() visible: boolean = false;
+  @Input() username? = '';
+  isDownloading = false;
+
+  constructor(private audioService: MediaAudioService) {}
+
+  downloadTrack() {
+    if (!this.track || !this.username) {
+      return;
+    }
+
+    this.isDownloading = true;
+    this.audioService.getUserAudioFile(this.username, this.track.filePath).subscribe({
+      next: (response: any) => {
+        // response contains url property with SAS URL
+        const sasUrl = response.url;
+        // Create a temporary link and trigger download
+        const link = document.createElement('a');
+        link.href = sasUrl;
+        link.download = this.track.filePath || 'audio-file';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        this.isDownloading = false;
+      },
+      error: () => {
+        this.isDownloading = false;
+      }
+    });
+  }
 }
