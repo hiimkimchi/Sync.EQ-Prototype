1d5fdb32260040fc7d88906fdd56d470c9189c8d
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-12-04 13:03:35 -0800

    updated jest testing to account for backend changes

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
